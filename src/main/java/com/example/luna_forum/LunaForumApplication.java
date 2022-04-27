3c06dd20bacb395696492708a6add2979e43ad15
AqualunaIsTaken <89570320+AqualunaIsTaken@users.noreply.github.com>
2022-04-27 11:19:55 +0200

    -Initial commit

---
+++ src/main/java/com/example/luna_forum/LunaForumApplication.java
@@ -0,0 +1,13 @@
+package com.example.luna_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunaForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunaForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/luna_forum/LunaForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luna_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunaForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
